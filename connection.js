b680d2677575ec050d1de6d95bedf100f102ea29
lokified <lsheldon645@gmail.com>
2022-08-02 11:06:26 +0300

    make change

--- connection.js
+++ connection.js
@@ -11,7 +11,7 @@ const pool = new Pool({
     // database: process.env.DB_NAME,
     // password: process.env.DB_PASSWORD,
     // port: process.env.DB_PORT
-    connectionString: process.env.DATABASE_URL,
+    connectionString: "postgres://zdbuzrspyyofop:1ff4fc31c19d06471716d70aa0d1e1d23cccca468873c69007459dc525737a46@ec2-54-85-56-210.compute-1.amazonaws.com:5432/d92i4rpe4tgg9j",
     ssl: {
         rejectUnauthorized: false,
     },
